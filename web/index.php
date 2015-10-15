f38b28728fdc7ada74940ad6a5666e52606e2642
Hannah Krier <hkrier@popsugar.com>
2015-10-15 11:00:24 -0700

    Table column name change

--- web/index.php
+++ web/index.php
@@ -38,15 +38,15 @@ $app->get('/new', function() use($app) {
 });
 
 $app->post('/save', function(Request $request) use($app) {
-  $sql = "INSERT INTO recipes (url, title, image_url, ingredients, description, date, rating_ben, rating_hannah)
-            VALUES (:url, :title, :image_url, :ingredients, :description, :date, :rating_ben, :rating_hannah)";
+  $sql = "INSERT INTO recipes (url, title, image_url, ingredients, directions, date, rating_ben, rating_hannah)
+            VALUES (:url, :title, :image_url, :ingredients, :directions, :date, :rating_ben, :rating_hannah)";
 
   $variables = [
     ':url' => $request->get('url'),
     ':title' => $request->get('title'),
     ':image_url' => $request->get('image_url'),
     ':ingredients' => $request->get('ingredients'),
-    ':description' => $request->get('description'),
+    ':directions' => $request->get('directions'),
     ':date' => $request->get('date'),
     ':rating_ben' => $request->get('rating_ben'),
     ':rating_hannah' => $request->get('rating_hannah'),
