4e3220f015dc40553818b3663dea935a63114f71
Hannah Krier <hkrier@popsugar.com>
2015-10-15 23:29:30 -0700

    one last thing then sleep

--- web/index.php
+++ web/index.php
@@ -92,9 +92,17 @@ $app->post('/create', function(Request $request) use($app) {
 
   $st = $app['pdo']->prepare($sql);
   $st->execute($variables);
-  $id = $app['pdo']->lastInsertId();
 
-  return $app->redirect('/view/' . $id);
+  // This is returning a really high number... why? shared db?
+  // $id = $app['pdo']->lastInsertId();
+
+  // Hate to have to do it this way...
+  $sql = "SELECT MAX(id) FROM recipes";
+  $st = $app['pdo']->prepare($sql);
+  $st->execute();
+  $id = $st->fetch(PDO::FETCH_NUM);
+
+  return $app->redirect('/view/' . $id[0]);
 });
 
 $app->post('/update', function(Request $request) use($app) {
