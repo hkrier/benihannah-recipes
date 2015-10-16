048a470ffaf386fbdcc11c0563e019b82ac11b54
Hannah Krier <hkrier@popsugar.com>
2015-10-15 17:36:19 -0700

    Bug fix

--- web/scripts/recipe.js
+++ web/scripts/recipe.js
@@ -1,6 +1,6 @@
 $(document).on('ready', function() {
     $('.rating-proxy').on('click', function() {
-        var input = $('input[' + $(this).data('input') + ']');
+        var input = $('input[name="' + $(this).data('input') + '"]');
         var rating = $(this).data('rating');
         input.val(rating);
         $(this).siblings('.rating').width(rating * 20 + '%');
