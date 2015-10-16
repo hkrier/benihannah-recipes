034080a7b77eca39961cbffc0acc2d594e8c21a2
Hannah Krier <hkrier@popsugar.com>
2015-10-15 17:29:45 -0700

    Add rating js

--- web/scripts/recipe.js
+++ web/scripts/recipe.js
@@ -1,1 +1,8 @@
-console.log('hi');
+$(document).on('ready', function() {
+    $('.rating-proxy').on('click', function() {
+        var input = $('input[' + $(this).data('input') + ']');
+        var rating = $(this).data('rating');
+        input.val(rating);
+        $(this).siblings('.rating').width(rating * 10 + '%');
+    });
+});
