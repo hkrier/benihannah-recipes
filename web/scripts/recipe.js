509c66ceb3d94d31cfb14a51803bca83934708d1
Hannah Krier <hkrier@popsugar.com>
2015-10-16 20:43:57 -0700

    will this work?

--- web/scripts/recipe.js
+++ web/scripts/recipe.js
@@ -14,4 +14,31 @@ $(document).on('ready', function() {
         var rating = input.val();
         $(this).siblings('.rating').width(rating * 20 + '%');
     });
+
+    $('#recipe-list th').on('click', function() {
+        if ($(this).hasClass('active')) {
+            $(this).toggleClass('desc');
+        }
+        else {
+            $('#recipe-list th').removeClass('.active');
+            $(this).addClass('active');
+        }
+
+        var $recipes = $('.recipes');
+        $recipes.remove();
+        var field = $(this).data('sort-field');
+        if ($(this).hasClass('desc')) {
+            // sort in reverse order
+            $recipes.sort(function(recipe1, recipe2) {
+                return $(recipe2).data(field) - $(recipe1).data(field);
+            });
+        }
+        else {
+            $recipes.sort(function(recipe1, recipe2) {
+                return $(recipe1).data(field) - $(recipe2).data(field);
+            });
+        }
+
+        $recipes.insertAfter('#recipe-list th:last-child');
+    });
 });
